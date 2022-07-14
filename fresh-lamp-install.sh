11d4039dd94e717f54ad08a75c4a4e857ff27cf2
Justin Bodnar <justin.bodnar@wilkes.edu>
2022-07-13 22:31:01 -0400

    Update fresh-lamp-install.sh
    updated commands to have less user-interaction

--- fresh-lamp-install.sh
+++ fresh-lamp-install.sh
@@ -1,26 +1,25 @@
 # fresh LAMP install script
 # by Justin Bodnar
 # June 17, 2021
+#
+# takes a fresh Ubuntu install, and installs a LAMP server with minimal user-terminal interaction
 
-# check for new distro
-apt-get update
-apt-get dist-upgrade
-
-# check for lib updates/upgrades
-apt-get update
-apt-get -y upgrade
-apt-get autoremove
-apt-get autoclean
+# updates
+apt dist-upgrade -y
+apt update -y
+apt upgrade -y
+apt autoremove -y
+apt autoclean -y
 
 # install apache
-apt-get install apache2
+apt-get install apache2 -y
 
-# install and configure mysql server
-apt-get install mysql-server
+# install and configure mysql server with user interaction
+apt-get install mysql-server -y
 mysql_secure_installation
 
 # install PHP
-apt-get install php libapache2-mod-php php-mysql
+apt-get install php libapache2-mod-php php-mysql -y
 
 # install certbot for free SSL
-apt-get install certbot python3-certbot-apache
+apt-get install certbot python3-certbot-apache -y
