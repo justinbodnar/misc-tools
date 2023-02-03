3df7e117a0e7665e160a513af3f568d24aefe29d
Justin Bodnar <justin.bodnar@wilkes.edu>
2023-02-03 11:18:05 -0500

    Update update-centos.sh

--- update-centos.sh
+++ update-centos.sh
@@ -1,4 +1,8 @@
+# update packages and delete old files
 yum upgrade -y
 package-cleanup --leaves
 package-cleanup --orphans
+
+# set centos to automatically update
 yum -y install yum-cron
+systemctl enable yum-cron.service
